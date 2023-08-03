161d164a1704ee4775701afa49ce72afc20af812
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-03 19:07:08 +0530

    Nav bar completed using bootstrap

--- brand-page/src/App.js
+++ brand-page/src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <>
-      <nav className='navbar'>
-        <div className="logo">
-          <img src='/images/brand_logo.png' alt='logo'/>
-        </div>
-        <div>
-          <ul id='links'>
-            <li><a href='#menu'>Menu</a></li>
-            <li><a href='#location'>Location</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#contact'>Contact</a></li>
-          </ul>
-        </div>
-        <div>
-          <button className='login' type='login'>Login</button>
-        </div>
-      </nav>
+      <Navbar/>      
+      <Slider/>
     </>
   );
 }
