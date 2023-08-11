836516402c481d7dcd8144452147f87ba530bf66
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-11 20:48:49 +0530

    contact form basic is redy

--- contact-us/src/component/Button/Button.js
+++ contact-us/src/component/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from './Button.module.css';
 
 function Button(props) {
-    const {isOutline,text,icon} = props; // distructuring
+    const {isOutline,text,icon} = props; // distructuring   
   return (
     <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
         {icon}
--- contact-us/src/component/ContactForm/ContactForm.js
+++ contact-us/src/component/ContactForm/ContactForm.js
@@ -18,8 +18,27 @@ function ContactForm() {
                 text="VIA MAIL FORM" 
                 icon={<HiMail fontSize="24px"/>}
             />
-        </div>
 
+            <form>
+                <div className={styles.form_control}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" />
+                </div>
+                <div className={styles.form_control}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="name" />
+                </div>
+                <div className={styles.form_control}>
+                    <label htmlFor="text">Text</label>
+                    <textarea name="text" rows="8"/>
+                </div>
+                <div style={{display: "flex", justifyContent: 'end',}}>
+                    <Button text="SUBMIT" />
+                </div>
+                
+            </form>
+        </div>
+            <img src="./images/contact.svg" alt="contact image"/>
         <div className={styles.Contact_image}>
 
         </div>
