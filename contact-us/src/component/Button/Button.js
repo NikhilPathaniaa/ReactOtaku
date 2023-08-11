86275862cac07c3dc8c0562e229b5b10ca8ece4b
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-11 20:24:49 +0530

    we here use distructuring

--- contact-us/src/component/Button/Button.js
+++ contact-us/src/component/Button/Button.js
@@ -1,10 +1,11 @@
 import styles from './Button.module.css';
 
 function Button(props) {
+    const {isOutline,text,icon} = props; // distructuring
   return (
-    <button className={props.isOutline ? styles.outline_btn : styles.primary_btn}>
-        {props.icon}
-        {props.text}
+    <button className={isOutline ? styles.outline_btn : styles.primary_btn}>
+        {icon}
+        {text}
     </button>
   )
 }
